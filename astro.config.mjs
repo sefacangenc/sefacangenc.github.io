a3e05779c5dd52270d1da6a8efd9142cddcb2c2c
sefa can  genc <sefacan19.sg@gmail.com>
2023-04-07 01:03:48 +0300

    edit
    edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sefacangenc.github.io/',
-
-	integrations: [mdx(), sitemap()],
+  site: 'https://sefacangenc.github.io/',
+  integrations: [mdx(), sitemap(), react()]
 });
