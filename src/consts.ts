5be9fbc051146228dcec885c8cb73e36ef4cc9a2
sefa can  genc <sefacan19.sg@gmail.com>
2023-05-02 21:04:13 +0300

    config.ts update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Personal Blog ';
+// export const HOMEPAGE_URL = 'https://www.sefacangenc.io/'
